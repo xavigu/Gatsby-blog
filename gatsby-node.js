f4f3b551cc32725465b19cfebe432967631adf23
xavigu <xavigu91@gmail.com>
2021-02-14 19:55:49 +0100

    Use gatsby-node file to create the slug (relative path) to access to the page of each md post in the application

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,24 @@
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  // obtain the createNodeField from the actions
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    // slug is the navigation path(url) that the browser use to navigate to the page required
+    // getNode is using to get the node object from a file
+    const slug = createFilePath({ 
+      node, 
+      getNode
+    });
+    console.log('sluuuug:', slug);
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    });
+  }
+  // console.log(node.internal.type)
+}
