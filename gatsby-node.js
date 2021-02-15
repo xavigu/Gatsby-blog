e73ac8eb3c91c18193b2455cd338321b73298656
xavigu <xavigu91@gmail.com>
2021-02-15 13:09:18 +0100

    Create template to blog-post pages obtaining the slug like context from gatsby-node file

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@
  *
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
-
+const path = require(`path`);
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -23,5 +23,32 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       value: slug,
     });
   }
-  // console.log(node.internal.type)
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  // graphql methos returns a promise
+  return graphql(`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({node}) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          slug: node.fields.slug
+        }
+      })
+    })
+  })
 }
