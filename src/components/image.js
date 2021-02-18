27af99a09b638f52d7967a36506f0dd5549fee85
xavigu <xavigu91@gmail.com>
2021-02-18 14:13:07 +0100

    Check tools

--- src/components/image.js
+++ src/components/image.js
@@ -14,7 +14,7 @@ import Img from 'gatsby-image';
  */
 
 const Image = () => {
-  const a = 'lo';
+  const a = 'los';
   const data = useStaticQuery(graphql`
     query {
       placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
